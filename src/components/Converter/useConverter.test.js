e77f2efa801ae52a6d043c17cb07470592060744
AlexDi <mirag0505@yandex.ru>
2023-03-27 12:51:29 +0800

    Plane hooks

---
+++ src/components/Converter/useConverter.test.js
@@ -0,0 +1,18 @@
+describe("when render", () => {
+  it.todo("RUB input should have a value equal to initial RUB amount");
+  it.todo("USD input should have a value equal to calculated USD amount");
+});
+
+describe("when called update method 'updateRUB'", () => {
+  it.todo("should update the RUB value ");
+  it.todo("should recalculate USD value");
+});
+
+describe("when called update method 'updateUsd'", () => {
+  it.todo("should update the USD value ");
+  it.todo("should recalculate RUB value");
+});
+
+describe("when re-render", () => {
+  it.todo("should update this value ");
+});
